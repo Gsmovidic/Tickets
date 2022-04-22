3ba23ba248183c4a7f909b35eb3558699f3f1623
Victor <victorzgames00@gmail.com>
2022-04-22 09:46:51 -0500

    Add project files.

---
+++ Tickets/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Tickets.Data.Entities;
+
+namespace Tickets.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        DbSet<Ticket> Tickets { get; set; }
+
+        DbSet<Entrance> Entrances { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Ticket>().HasIndex(t=>t.Id).IsUnique();
+            modelBuilder.Entity<Entrance>().HasIndex(e=>e.Id).IsUnique();
+            
+        }
+    }
+}
